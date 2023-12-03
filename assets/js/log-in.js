357d6d9f0dcd18408652dca958382f0d25e17db2
Saksham Bedi <bedis3@myumanitoba.ca>
2023-12-02 20:46:24 -0600

    fixed the link for to go to the index.html

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,24 +1,13 @@
-const inputs = document.querySelectorAll(".input");
-
-function focusFunx() {
-  let parent = this.parentNode.parentNode;
-  parent.classList.add("focus");
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunx);
-});
-
-
-
-// Last page
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
-const submitBtn = document.getElementById('submitBtn');
+const submitBtn = document.getElementById('submitBtn')
+  ;
 
 function checkInputs() {
+  event.preventDefault()
   if (input1.value == 'admin' && input2.value == 'admin') {
-    window.location.href= "";
+
+    window.location = "./../index.html";
   } else {
     alert("Wrong ID or Password. Please try again.");
   }
