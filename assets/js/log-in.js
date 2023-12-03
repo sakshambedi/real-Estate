632a93dfcf9d718ef9324ddddddde1223e7a7148
Junsig, Kim <consilience7788@gmail.com>
2023-12-02 22:04:04 -0600

    test on mac

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,24 +1,27 @@
-const inputs = document.querySelectorAll(".input");
+// const inputs = document.querySelectorAll(".input");
 
-function focusFunx() {
-  let parent = this.parentNode.parentNode;
-  parent.classList.add("focus");
-}
+// function focusFunx() {
+//   let parent = this.parentNode.parentNode;
+//   parent.classList.add("focus");
+// }
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunx);
-});
+// inputs.forEach((input) => {
+//   input.addEventListener("focus", focusFunx);
+// });
 
 
 
 // Last page
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
-const submitBtn = document.getElementById('submitBtn');
+const submitBtn = document.getElementById('submitBtn')
+;
 
 function checkInputs() {
+  event.preventDefault()
   if (input1.value == 'admin' && input2.value == 'admin') {
-    window.location.href= "";
+    
+    window.location = "file:///C:/Users/consi/Desktop/newnewbranchv2/real-Estate/index.html";
   } else {
     alert("Wrong ID or Password. Please try again.");
   }
